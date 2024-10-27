4f9e71d83a3b75bb781dacd08ea7f56f4c2c91c3
ihsanye <ihsany98@gmail.com>
2024-10-27 12:53:05 +0300

    create and add todo func

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
 import './App.css'
 import TodoCreate from './components/TodoCreate'
 import TodoList from './components/TodoList'
 
 function App() {
 
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo])
+  }
+
+  console.log(todos)
+
   return (
     <div className='container'>
-      <TodoCreate />
+      <TodoCreate onCreateTodo={addTodo} />
       <TodoList />
     </div>
   )
--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function TodoCreate() {
+function TodoCreate({ onCreateTodo }) {
+
+    const [newTodo, setNewTodo] = useState('');
+
+    const createTodo = () => {
+        if (!newTodo) return;
+        const request = {
+            id: Math.floor(Math.random() * 999999),
+            content: newTodo
+        }
+        onCreateTodo(request)
+    }
+
     return (
         <div className="todo-create">
-            <input id='todo-input' type="text" placeholder='Todo Giriniz' />
-            <button id='todo-create-btn'>Todo Olustur</button>
+            <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} id='todo-input' type="text" placeholder='Todo Giriniz' />
+            <button onClick={createTodo} id='todo-create-btn'>Todo Olustur</button>
         </div>
     )
 }
